f114f4a777fa283e6a69db5832c72c4704682bf6
Basil Gren <bas.gren@gmail.com>
2015-03-05 03:02:23 +0300

    Implement navigation to engine class.

--- src/net/bitpot/railways/models/routes/EngineRoute.java
+++ src/net/bitpot/railways/models/routes/EngineRoute.java
@@ -4,6 +4,9 @@ import com.intellij.openapi.module.Module;
 import net.bitpot.railways.gui.RailwaysIcons;
 import net.bitpot.railways.models.Route;
 import net.bitpot.railways.models.requestMethods.RequestMethod;
+import net.bitpot.railways.utils.RailwaysPsiUtils;
+import net.bitpot.railways.utils.RailwaysUtils;
+import org.jetbrains.plugins.ruby.ruby.lang.psi.holders.RContainer;
 
 import javax.swing.*;
 
@@ -32,5 +35,15 @@ public class EngineRoute extends Route {
     @Override
     public Icon getActionIcon() {
         return RailwaysIcons.NODE_MOUNTED_ENGINE;
+    }
+
+    
+    @Override
+    public void navigate(boolean requestFocus) {
+        RContainer container = RailwaysPsiUtils.
+                findClassOrModule(engineClass, getModule().getProject());
+        
+        if (container != null)
+            container.navigate(requestFocus);
     }
 }
