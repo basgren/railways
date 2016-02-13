05db15c38abfbac99f50d7fb1cd4a7bb5f622d6d
Basil Gren <bas.gren@gmail.com>
2016-02-13 23:11:22 +0300

    Implement copy for treeview

--- src/net/bitpot/railways/ui/tree/RoutesTree.java
+++ src/net/bitpot/railways/ui/tree/RoutesTree.java
@@ -1,9 +1,14 @@
 package net.bitpot.railways.ui.tree;
 
+import com.intellij.openapi.actionSystem.DataProvider;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.ui.treeStructure.Tree;
+import net.bitpot.railways.models.Route;
 import net.bitpot.railways.ui.MainPanel;
 import net.bitpot.railways.models.RouteNode;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
@@ -14,12 +19,15 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Basil Gren
  *         on 28.12.2014.
  */
-public class RoutesTree extends Tree {
+public class RoutesTree extends Tree implements DataProvider {
 
     private MainPanel parentPanel;
 
@@ -86,4 +94,39 @@ public class RoutesTree extends Tree {
         node.getRoute().navigate(false);
     }
 
+    @Nullable
+    @Override
+    public Object getData(@NonNls String dataId) {
+        if (PlatformDataKeys.SELECTED_ITEMS.is(dataId))
+            return getSelectedRoutes();
+
+        if (PlatformDataKeys.SELECTED_ITEM.is(dataId))
+            return getSelectedRoute();
+
+        return null;
+    }
+
+    @Nullable
+    public Route getSelectedRoute() {
+        RouteNode node = (RouteNode)(getSelectionPath().getLastPathComponent());
+
+        return node.getRoute();
+    }
+
+    @Nullable
+    public Route[] getSelectedRoutes() {
+        List<Route> result = new ArrayList<Route>();
+
+        TreePath[] selected = getSelectionPaths();
+        if (selected == null)
+            return null;
+
+        for(TreePath path: selected) {
+            Route route = ((RouteNode)path.getLastPathComponent()).getRoute();
+            if (route != null)
+                result.add(route);
+        }
+
+        return result.toArray(new Route[result.size()]);
+    }
 }
